892ef4e030af66cab26804d368e02db358c0b4e6
Tanner Marshall <tanner.s.marshall@gmail.com>
2021-04-25 22:13:04 -0400

    Added box and flex components and rollup config

---
+++ packages/flexbox/src/Box.svelte
@@ -0,0 +1,42 @@
+
+<script>
+  import { getContext } from 'svelte';
+  import { css as emotionCss } from '@emotion/css'
+  import { get, css } from '@styled-system/css';
+  import { space } from '@styled-system/space';
+  import { layout } from '@styled-system/layout';
+  import { typography } from '@styled-system/typography';
+  import { color } from '@styled-system/color';
+  import { flexbox } from '@styled-system/flexbox';
+
+  const sx = (sxProps, theme) => css(sxProps)(theme);
+  const variant = ({
+    theme,
+    variant,
+    tx = 'variants',
+  }) => css(
+      get(theme, tx + '.' + variant,
+        get(theme, variant)
+      )
+    )(theme)
+
+  const theme = getContext('theme');
+
+  const className = emotionCss(
+    sx({
+      boxSizing: 'border-box',
+      ...variant({ theme, variant: $$props.variant }),
+      ...space($$props),
+      ...layout($$props),
+      ...typography($$props),
+      ...color($$props),
+      ...flexbox($$props),
+      ...($$props.sx),
+    }, theme)
+  );
+
+</script>
+
+<div class={`${className}`}>
+  <slot></slot>
+</div>
---
+++ packages/flexbox/src/Flex.svelte
@@ -0,0 +1,8 @@
+
+<script>
+  import Box from './Box.svelte';
+</script>
+
+<Box {...$$props} display="flex">
+  <slot></slot>
+</Box>
