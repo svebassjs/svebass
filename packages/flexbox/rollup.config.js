51b110252e9fe1246461d26365ab0d14f5b10a36
Tanner Marshall <tanner.s.marshall@gmail.com>
2021-04-27 12:42:39 -0400

    Updated Box component to typescript and added types

--- packages/flexbox/rollup.config.js
+++ packages/flexbox/rollup.config.js
@@ -1,6 +1,8 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import sveltePreprocess from 'svelte-preprocess';
+
 import pkg from './package.json';
 
 const name = pkg.name
@@ -15,7 +17,9 @@ const plugins = [
     browser: true,
   }),
   commonjs(),
-  svelte(),
+  svelte({
+    preprocess: sveltePreprocess(),
+  }),
 ];
 
 const output = [
