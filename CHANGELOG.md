f86b1236dbfe0833f7a47192f02bd313aa132092
Tanner Marshall <tanner.s.marshall@gmail.com>
2021-04-27 15:32:01 -0400

    Added generic svebass package that will export all components

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+# Changelog
+
+## 1.1.3 2021-04-27
+
+### Added
+- Generic svebass package with all components
+- Generic README.md
+- LICENCE.md
+
+## 1.1.2 2021-04-27
+
+### Added
+- Typescript support for flexbox components
+- Changelog
+- License
+
+### Changed
+- Rollup config to use svelte preprocess
---
+++ packages/svebass/rollup.config.js
@@ -0,0 +1,42 @@
+import svelte from 'rollup-plugin-svelte';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { typescript } from 'svelte-preprocess';
+
+import pkg from './package.json';
+
+const name = pkg.name
+  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
+  .replace(/^\w/, (m) => m.toUpperCase())
+  .replace(/-\w/g, (m) => m[1].toUpperCase());
+
+// const dev = process.env.NODE_ENV === 'development'
+
+const plugins = [
+  resolve({
+    browser: true,
+  }),
+  commonjs(),
+  svelte({
+    preprocess: typescript({
+      tsconfigDirectory: '../../',
+    }),
+  }),
+];
+
+const output = [
+  { file: `${pkg.module}`, format: 'es' },
+  { file: `${pkg.main}`, format: 'umd', name },
+];
+
+const external = Object.keys(pkg.dependencies);
+
+export default {
+  input: './src/index.js',
+  // https://stackoverflow.com/questions/64152594/should-svelte-libraries-include-external-svelte-in-rollup-config-js-func
+  // Ignoring svelte and svelte/internal bc otherwise new Svelte components are generated which
+  // appears to break (get/set)Context
+  external: [...external, 'svelte', 'svelte/internal'],
+  output,
+  plugins,
+};
